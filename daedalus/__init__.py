ae8b38b5df0a0631b93768c27aff13c0d9c6d7b6
Nick Setzer <nicksetzer@gmail.com>
2020-06-14 19:39:15 -0400

    make bytecode an optional dependency

--- daedalus/__init__.py
+++ daedalus/__init__.py
@@ -1,6 +1,9 @@
 
-
-from .loader import JavascriptFinder
-
-# call to enable importing javascript from python
-install = JavascriptFinder.install
+# TODO: this is kind of a bad idea, instead the user should
+# install the loader prior to running the interpreter.
+#try:
+#    from .loader import JavascriptFinder
+#    # call to enable importing javascript from python
+#    install = JavascriptFinder.install
+#except ImportError as e:
+#    pass
